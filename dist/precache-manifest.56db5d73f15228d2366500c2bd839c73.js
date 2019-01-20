4128a091b27d688f1f905c12a2ba07de20bfaeef
Dennis Moon <dmoon396@hotmail.com>
2019-01-20 08:27:40 -0600

    Initial checkin

---
+++ dist/precache-manifest.56db5d73f15228d2366500c2bd839c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d560aba4dab5ec01dddd",
+    "url": "/js/about-legacy.43739a75.js"
+  },
+  {
+    "revision": "6e7607d4f652ba6aef88",
+    "url": "/css/app.f46bd9d7.css"
+  },
+  {
+    "revision": "6e7607d4f652ba6aef88",
+    "url": "/js/app-legacy.e9c4eb39.js"
+  },
+  {
+    "revision": "d4d5be6b96cf8cf6af31",
+    "url": "/js/chunk-vendors-legacy.f5253a30.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "596c01b4b76ab5e1475921279195d507",
+    "url": "/index.html"
+  }
+];
---
+++ dist/precache-manifest.7c2119f4f559ba5da1df60acafc48463.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d560aba4dab5ec01dddd",
+    "url": "/js/about.43739a75.js"
+  },
+  {
+    "revision": "fb368c85cc05ae93d9d4",
+    "url": "/css/app.f46bd9d7.css"
+  },
+  {
+    "revision": "fb368c85cc05ae93d9d4",
+    "url": "/js/app.2e9532a1.js"
+  },
+  {
+    "revision": "97149a4cd22410b4a0a3",
+    "url": "/js/chunk-vendors.d81cba7c.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "86661b26da5b5f217c55c405f2904846",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
